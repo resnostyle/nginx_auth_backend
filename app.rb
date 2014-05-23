72872e20730a967bf6f9800d2a6a78fa8c8784f3
Bryan Pearson <bpearson@reverbnation.com>
2014-05-23 11:42:12 -0400

    testing allow domain

--- app.rb
+++ app.rb
@@ -25,6 +25,7 @@ class Auth < Sinatra::Base
 
     # If authorized, serve request
     if url = authorized?(request.host)
+      headers = "X-Remote-User" => session[:email]
       headers "X-Reproxy-URL" => url + request.fullpath
       headers "X-Accel-Redirect" => "/reproxy"
       return ""
@@ -35,15 +36,33 @@ class Auth < Sinatra::Base
   end
 
   # Block that is called back when authentication is successful
-  process = lambda do
+ process = lambda do
     auth = request.env['omniauth.auth']
     session[:logged] = true
     session[:provider] = auth.provider
     session[:uid] = auth.uid
     session[:name] = auth.info.name
     session[:email] = auth.info.email
+    if request.env.has_key? 'HTTP_X_FORWARDED_FOR'
+      session[:remote_ip] = request.env['HTTP_X_FORWARDED_FOR']
+    else
+      session[:remote_ip] = request.env['HTTP_X_REAL_IP']
+    end
     redirect request.env['omniauth.origin'] || "/"
   end
+#  process = lambda do
+#    auth = request.env['omniauth.auth']
+#    session[:logged] = true
+#    session[:provider] = auth.provider
+#    session[:uid] = auth.uid
+#    session[:name] = auth.info.name
+#    session[:email] = auth.info.email
+#    if request.env.has_key? 'HTTP_X_FORWARDED_FOR']
+#	else
+#	session[:remote_ip] = request.evn['HTTP_X_REAL_IP']
+#    end
+#    redirect request.env['omniauth.origin'] || "/"
+#  end
 
   get '/auth/:name/callback', &process
   post '/auth/:name/callback', &process
@@ -67,13 +86,33 @@ class Auth < Sinatra::Base
     end
   end
 
-  # Return internal URL or false if unauthorized
-  def authorized?(host)
+ def authorized?(host)
+    authorized = false
     # Check whether the email address is authorized
-    if settings.allowed_email.include?(session[:email]) & settings.routing.key?(host)
+    if ! session.has_key? :email
+      return false
+    end
+    split_email_address = session[:email].split('@')
+    if defined? settings.allowed_email_domains and settings.allowed_email_domains.include? split_email_address.last
+      authorized = true
+    elsif defined? settings.allowed_email and settings.allowed_email.include? session[:email]
+      authorized = true
+    end
+    if authorized == true and settings.routing.has_key? host
       return settings.routing[host]
     else
       return false
     end
   end
+
+  # Return internal URL or false if unauthorized
+#  def authorized?(host)
+#   authorized = false
+    # Check whether the email address is authorized
+#    if settings.allowed_email.include?(session[:email]) & settings.routing.key?(host)
+#      return settings.routing[host]
+#    else
+#      return false
+#    end
+#  end
 end
